917ba201757a3bfc0f65222da3d43b3ba849dd58
Raihan Dzaky <sraihandzaky@gmail.com>
2020-12-28 20:30:56 +0700

    AnimatedHorizontalFlatList

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import AnimatedHorizontalFlatList from './src/Screens/AnimatedHorizontalCarousel';
+
+export default function App() {
+  return (
+    <>
+      <AnimatedHorizontalFlatList />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: #FFFF,
+  // },
+});
